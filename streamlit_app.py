3976fb77cf2d1781bc98b39efdafea7c0bcb6ebb
Selvakumar R <110803067+selvakumar83@users.noreply.github.com>
2026-03-20 23:21:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,8 +62,7 @@ if "login" not in st.session_state:
 # -------------------------
 if not st.session_state.login:
 
-st.markdown("<h3 style='text-align:center;'>Department of Artificial Intelligence and Data Science</h3>", unsafe_allow_html=True)
-st.markdown("<h4 style='text-align:center;'>III Year – Section A</h4>", unsafe_allow_html=True)
+
 
     st.markdown("<h1 class='title'>🔐 Student Login</h1>", unsafe_allow_html=True)
 
