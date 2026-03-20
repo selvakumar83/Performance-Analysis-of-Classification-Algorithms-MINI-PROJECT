7036cd9b65b71c0002f3c06bbc5f8d845ab357ff
Selvakumar R <110803067+selvakumar83@users.noreply.github.com>
2026-03-20 23:33:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,8 +64,8 @@ if not st.session_state.login:
 
  
 
-    st.markdown("<h1 class='title'>🔐 Student Login</h1>", unsafe_allow_html=True)
-     st.markdown("### <h1 class='title'>[AI&DS III-A]</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 class='title'>🔐 Student Login [AI&DS III-A]</h1>", unsafe_allow_html=True)
+     
 
     reg_no = st.text_input("Enter Your Roll Number")
 
